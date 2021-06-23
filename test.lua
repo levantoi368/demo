dd41fbcf9508bb35ac4c1cdac073cedfef345aa0
le van toi <56533470+levantoi368@users.noreply.github.com>
2021-06-23 15:13:34 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,25 +1,5 @@
-function waitcolor(...)
-	usleep(500000)
+usleep(500000)
 	local tab = {...}
 	local cont = tab[#tab - 1];
 	local value = tab[#tab]
 	local j = 1
-	repeat
-		local i = 1;
-		repeat
-			local x = tab[i]
-			local y = tab[i+1]
-			local color = tab[i+2]
-			i = i + 3
-			if (getColor(x, y) == color) then
-				usleep(500000)
-				return x, y
-			end
-		until(i == #tab - 1);
-		j = j + 1
-		if (j == cont) then
-			return 0
-		end
-		usleep(500000)
-	until(j == cont)
-end
